f668ab25134e7542b049b5ac476542e7a1aae591
kavaliushasree16-wq <kavaliushasree16@gmail.com>
2026-07-31 10:43:13 +0530

    Time: 16 ms (20.9%), Space: 46.6 MB (25.76%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,31 +1,26 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
         if (s.length() != t.length()) {
             return false;
         }
-
-        char[] arr = t.toCharArray();
-
-        for (int i = 0; i < s.length(); i++) {
-
-            boolean found = false;
-
-            for (int j = 0; j < arr.length; j++) {
-
-                if (arr[j] != '#'&& s.charAt(i) == arr[j]) {
-                    arr[j] = '#';   // mark as used
-                    found = true;
-                    break;
-                }
-
-            }
-
-            if (!found) {
-                return false;
-            }
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
-        return true;
+        for(char ch:t.toCharArray()){
+            if(!map.containsKey(ch)){
+                return false;
+            }
+
+            map.put(ch,map.get(ch)-1);
+
+            if(map.get(ch)==0){
+                map.remove(ch);
+            }
+
+
+        }
+        return map.isEmpty();
     }
 }
