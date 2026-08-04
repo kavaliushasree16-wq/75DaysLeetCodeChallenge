eb496881ddde99cf998646581439e45ea6407a5c
kavaliushasree16-wq <kavaliushasree16@gmail.com>
2026-08-04 18:45:44 +0530

    Time: 2 ms (99.32%), Space: 44.6 MB (49.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String lower = s.toLowerCase();
-       StringBuilder original = new StringBuilder();
-StringBuilder reversed = new StringBuilder();
+        int left=0;
+        int right = s.length() - 1;
+        while (left < right) {
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
 
-        for (int i = 0; i < lower.length(); i++) {
-            char ch=lower.charAt(i);
-            if ((ch>= 'a' && ch<= 'z') || (ch >= '0' &&ch <= '9')) {
-                original.append(ch);
+             while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
             }
-        }
-        for (int i = lower.length() - 1; i >= 0; i--) {
-           char a=lower.charAt(i);
-            if ((a>= 'a' && a<= 'z') || (a >= '0' &&a <= '9')) {
-                reversed.append(a);
+
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                return false;
             }
+
+            left++;
+            right--;
         }
-        if (original.toString().equals(reversed.toString())) {
-            return true;
-        } else {
-            return false;
-        }
+
+        return true;
 
     }
 }
