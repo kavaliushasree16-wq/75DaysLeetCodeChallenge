8ecdf0aeb48c818562bbf1b909a39af0fa506285
kavaliushasree16-wq <kavaliushasree16@gmail.com>
2026-08-12 10:40:16 +0530

    Time: 10 ms (54.79%), Space: 46 MB (78.98%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,25 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-         
-        int[] count = new int[26]; // store frequency
+        int count[] = new int[26];
         int left = 0;
-        int maxFreq = 0;
         int maxLength = 0;
+        int maxFreq = 0;
 
         for (int right = 0; right < s.length(); right++) {
-
-            count[s.charAt(right) - 'A']++;
-            maxFreq = Math.max(maxFreq, count[s.charAt(right) - 'A']);
- 
-            while ((right - left + 1) - maxFreq > k) {
+            int index = s.charAt(right) - 'A';
+            count[index]++;
+            maxFreq = Math.max(maxFreq, count[index]);
+            int windowLength = right - left + 1;
+            if (windowLength - maxFreq > k) {
                 count[s.charAt(left) - 'A']--;
                 left++;
             }
 
             maxLength = Math.max(maxLength, right - left + 1);
+
         }
 
         return maxLength;
-     
     }
 }
