19da0d798f1ff7a7ce26899eee8fe375a25468eb
kavaliushasree16-wq <kavaliushasree16@gmail.com>
2026-08-12 11:39:34 +0530

    Time: 6 ms (87.83%), Space: 44.1 MB (60.53%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()){
+            return false;
+        }
+        int count1[]=new int[26];
+        int count2[]=new int[26];
+
+        for(int i=0;i<s1.length();i++){
+            count1[s1.charAt(i)-'a']++;
+        }
+        int k=s1.length();
+
+        for(int i=0;i<s2.length();i++){
+            count2[s2.charAt(i)-'a']++;
+
+            if(i>=k){
+                count2[s2.charAt(i-k)-'a']--;
+            }
+
+            if(Arrays.equals(count1, count2)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
