d79152214b9a8b7e4c289a69f95f7720287aa499
kavaliushasree16-wq <kavaliushasree16@gmail.com>
2026-08-19 20:11:47 +0530

    Time: 5 ms (31.75%), Space: 69.8 MB (10.76%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,17 +1,18 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-       double maxavg, windowsum=0;
+        double maxavg = 0, windowsum = 0;
 
-       for(int i=0;i<k;i++){
-        windowsum+=nums[i];
-       }
-       maxavg=windowsum/k;
+        for (int i = 0; i < k; i++) {
+            windowsum += nums[i];
+        }
+        maxavg = windowsum / k;
 
-       for(int i=k;i<nums.length;i++){
-        windowsum+=nums[i];
-        windowsum-=nums[i-k];
-        maxavg=Math.max(maxavg,windowsum/k);
-       }
-       return maxavg;
+        for (int i = k; i < nums.length; i++) {
+            windowsum += nums[i];
+            windowsum -= nums[i - k];
+            maxavg = Math.max(maxavg, windowsum / k);
+
+        }
+        return maxavg;
     }
 }
