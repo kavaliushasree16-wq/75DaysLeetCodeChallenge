484b5ce5c484f49e605536673a64b78dfe8c3c6e
kavaliushasree16-wq <kavaliushasree16@gmail.com>
2026-08-20 15:43:22 +0530

    Time: 71 ms (5.07%), Space: 48.6 MB (8.39%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
 
-        int ans[] = new int[k];
-        for (int i = 0; i < k; i++) {
-            int maxfreq = -1;
-            int element = -1;
-            for (int key : map.keySet()) {
-                if (map.get(key) > maxfreq) {
-                    maxfreq = map.get(key);
-                    element = key;
+        int ans[]=new int[k];
+        for(int i=0;i<k;i++){
+            int maxfreq=-1;
+            int element=-1;
+            for(int key:map.keySet()){
+                if(map.get(key)>maxfreq){
+                    maxfreq=map.get(key);
+                    element=key;
                 }
+                
             }
-
-            ans[i] = element;
-            map.put(element, -1);
-
+            ans[i]=element;
+                map.put(element,-1);
         }
         return ans;
-
     }
 }
