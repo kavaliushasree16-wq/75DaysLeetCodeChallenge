9598e715443a31f2ba6b0ac157131be13fff603f
kavaliushasree16-wq <kavaliushasree16@gmail.com>
2026-08-24 22:10:21 +0530

    Time: 6 ms (87.2%), Space: 45.4 MB (45.76%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -4,23 +4,24 @@ class Solution {
 
         for (String token : tokens) {
             if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
-                int a = stack.pop();
-                int b = stack.pop();
 
-                if (token.equals("+")) {
-                    stack.push(b + a);
-                } else if (token.equals("-")) {
-                    stack.push(b - a);
-                } else if (token.equals("*")) {
-                    stack.push(b * a);
-                } else {
-                    stack.push(b / a);
+                int a=stack.pop();
+                int b=stack.pop();
+
+                if(token.equals("+")){
+                    stack.push(b+a);
+                }else if(token.equals("-")){
+                    stack.push(b-a);
+                }else if(token.equals("*")){
+                    stack.push(b*a);
+                }else{
+                    stack.push(b/a);
                 }
 
             } else {
                 stack.push(Integer.parseInt(token));
             }
         }
-        return stack.pop();
+        return stack.peek();
     }
 }
