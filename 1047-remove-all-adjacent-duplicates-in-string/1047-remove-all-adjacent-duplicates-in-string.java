4a834cc3ea9feb71e3daf90a034d8066a2322993
kavaliushasree16-wq <kavaliushasree16@gmail.com>
2026-08-24 19:45:23 +0530

    Time: 11 ms (91.24%), Space: 47.3 MB (38.22%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -2,7 +2,7 @@ class Solution {
     public String removeDuplicates(String s) {
        StringBuilder sb=new StringBuilder();
        for(char ch: s.toCharArray()){
-        if(sb.length()>0 && sb.charAt(sb.length()-1)-ch==0){
+        if(sb.length()>0 && sb.charAt(sb.length()-1)==ch){
             sb.deleteCharAt(sb.length()-1);
         }else{
             sb.append(ch);
