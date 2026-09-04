3df4f031d830656daa1dcae3f6bbeef8a68dc23e
kavaliushasree16-wq <kavaliushasree16@gmail.com>
2026-09-04 14:05:05 +0530

    Time: 0 ms (100%), Space: 47.7 MB (14.35%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int result=0;
+        for(int i=0;i<=nums.length;i++){
+            result=result^i;
+        }
+      
+        for(int num:nums){
+          result^=num;
+        }
+
+        return result;
+    }
+}
